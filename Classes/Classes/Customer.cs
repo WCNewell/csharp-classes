d81d5535b2895edbc57de929250137fc7dbfb1ab
W. Clark Newell <clark@clarknewell.tech>
2019-11-09 15:07:31 -0700

    initial commit

---
+++ Classes/Classes/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+        
+        // vv Technically these are redundant to the above code and don't 
+        public Customer(int id)
+            :this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            :this(id)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    //public class Person
+    //{
+    //    public string Name;
+
+    //    public void Introduce(string to)
+    //    {
+    //        Console.WriteLine("Hi {0}, I am {1}", to, Name);
+    //        Console.ReadLine();
+    //    }
+
+    //    public static Person Parse(string str)
+    //    {
+    //        var person = new Person();
+    //        person.Name = str;
+
+    //        return person;
+    //    }
+    //}
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var person = Person.Parse("John");
+    //        person.Introduce("Clark");
+    //    }
+    //}
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer(1, "John");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+            Console.ReadLine();
+
+        }
+
+    }
+}

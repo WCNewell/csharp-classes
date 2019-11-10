b1584a15779a8a84412b697b86e053a7e0232502
W. Clark Newell <clark@clarknewell.tech>
2019-11-10 13:44:56 -0700

    studying method modifiers

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -4,29 +4,45 @@ namespace Methods
 {
     class Program
     {
-        public class Point
+        private static readonly bool result;
+
+        static void Main(string[] args)
         {
-            public int X;
-            public int Y;
+            //var number = int.Parse("abc");
 
-            public Point(int x, int y)
+            int number;
+            int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion Failed");
+
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoints()
+        {
+            try
             {
-                this.X = x;
-                this.Y = y;
+                var point = new Point(10, 20);
+                point.Move(new Point(40, 60));
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
             }
-
-            public void Move(int x, int y)
+            catch (Exception)
             {
-                this.X = x;
-                this.Y = y;
+                Console.WriteLine("An unexpected error occured");
             }
-
-            public void Move(Point newLocation)
-            {
-                this.X = newLocation.X;
-                this.Y = newLocation.Y;
-            } 
-
         }
     }
 
